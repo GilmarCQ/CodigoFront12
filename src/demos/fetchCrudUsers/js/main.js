4e86a32c408d095837b4362b9d372c617fc5320f
GilmarCQ <gacampanaq@gmail.com>
2022-01-27 00:38:30 -0500

    CRUD Fetch

--- src/demos/fetchCrudUsers/js/main.js
+++ src/demos/fetchCrudUsers/js/main.js
@@ -1,15 +1,24 @@
-const form = document.querySelector('form');
+const form = document.querySelector('#formAdd');
 const name = form.querySelector('#name');
 const lastname = form.querySelector('#lastname');
 const country = form.querySelector('#country');
 const mail = form.querySelector('#mail');
 const photo = form.querySelector('#photo');
 const btnAdd = form.querySelector('#btn-add');
-const btnClean = form.querySelector('#btn-clean');
+const modal = document.querySelector('#modal1');
+const formModal = modal.querySelector('form');
 const listUsersRow = document.querySelector('#list-users');
-
 const URL_API = 'https://61ef3de0d593d20017dbb3c1.mockapi.io';
-
+const body = document.querySelector('body');
+const spinner = document.querySelector('.spinner');
+const showSpinner = () => {
+    body.style.overflow = 'hidden';
+    spinner.classList.add('show');
+}
+const hideSpinner = () => {
+    body.style.overflow = 'visible';
+    spinner.classList.remove('show');
+}
 const getUser = () => {
     return {
         'name': name.value,
@@ -19,6 +28,36 @@ const getUser = () => {
         'mail': mail.value,
         'description': description.value
     };
+}
+const getUserModal = () => {
+    return {
+        'name': formModal.querySelector('#nameModal').value,
+        'lastname': formModal.querySelector('#lastnameModal').value,
+        'photo': formModal.querySelector('#photoModal').value,
+        'country': formModal.querySelector('#countryModal').value,
+        'mail': formModal.querySelector('#mailModal').value,
+        'description': formModal.querySelector('#descriptionModal').value
+    };
+}
+const cleanForm = () => {
+    form.querySelector('#name').value = '';
+    form.querySelector('#name').classList.remove('valid');
+    form.querySelector('#name').parentElement.querySelector('label').classList.remove('active');
+    form.querySelector('#lastname').value = '';
+    form.querySelector('#lastname').classList.remove('valid');
+    form.querySelector('#lastname').parentElement.querySelector('label').classList.remove('active');
+    form.querySelector('#country').value = '';
+    form.querySelector('#country').classList.remove('valid');
+    form.querySelector('#country').parentElement.querySelector('label').classList.remove('active');
+    form.querySelector('#mail').value = '';
+    form.querySelector('#mail').classList.remove('valid');
+    form.querySelector('#mail').parentElement.querySelector('label').classList.remove('active');
+    form.querySelector('#photo').value = '';
+    form.querySelector('#photo').classList.remove('valid');
+    form.querySelector('#photo').parentElement.querySelector('label').classList.remove('active');
+    form.querySelector('#description').value = '';
+    form.querySelector('#description').classList.remove('valid');
+    form.querySelector('#description').parentElement.querySelector('label').classList.remove('active');
 }
 const saveUser = (user, callback) => {
     fetch(`${URL_API}/users`, {
@@ -36,25 +75,15 @@ const saveUser = (user, callback) => {
             console.log('Error ', e);
         });
 }
-
 const addCard = (user) => {
     const nodeCol = document.createElement('div');
     nodeCol.classList.add('col', 's12', 'm4', 'l4', 'xl3');
-    // const nodeCard = document.createElement('div');
-    // nodeCard.classList.add('card', 'hoverable');
-    // const nodeCardImage = document.createElement('div');
-    // nodeCardImage.classList.add('card-image');
-    // const nodeCardImageImg = document.createElement('img');
-    // nodeCardImageImg.classList.add('circle');
-    // nodeCardImageImg.src = user.photo;
-
-    // nodeCardImage.appendChild(nodeCardImageImg);
     nodeCol.innerHTML = `
         <div class="card hoverable">
             <div class="card-image">
                 <img
                     class="circle"
-                    src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=200"
+                    src="${user.photo}"
                     alt=""
                 />
                 <a class="btn-floating halfway-fab waves-effect waves-light red hoverable" onclick="deleteUser(${user.id})">
@@ -70,47 +99,21 @@ const addCard = (user) => {
                 <p>${user.description}</p>
             </div>
         </div>`;
-    // nodeCard.appendChild(nodeCardImage);
-
     const divider = document.createElement('div');
     divider.classList.add('divider');
-
-    // nodeCard.appendChild(divider);
-
-    // const nodeCardContent = document.createElement('div');
-    // nodeCardContent.classList.add('card-content');
-    // const nodeCardPName = document.createElement('p');
-    // nodeCardPName.classList.add('truncate');
-    // nodeCardPName.innerText = `${user.name} ${user.lastname}`;
-    // const nodeCardPCountry = document.createElement('p');
-    // nodeCardPCountry.classList.add('truncate');
-    // nodeCardPCountry.innerText = `${user.country}`;
-    // const nodeCardPMail = document.createElement('p');
-    // nodeCardPMail.classList.add('truncate');
-    // nodeCardPMail.innerText = `${user.mail}`;
-    // const nodeCardPDescription = document.createElement('p');
-    // nodeCardPDescription.classList.add('truncate');
-    // nodeCardPDescription.innerText = `${user.description}`;
-    // nodeCardContent.appendChild(nodeCardPName);
-    // nodeCardContent.appendChild(nodeCardPCountry);
-    // nodeCardContent.appendChild(nodeCardPMail);
-    // nodeCardContent.appendChild(nodeCardPDescription);
-    // nodeCard.appendChild(nodeCardContent);
-    // nodeCol.appendChild(nodeCard);
-
     listUsersRow.appendChild(nodeCol);
 }
-
 const createUser = (event) => {
     event.preventDefault();
+    showSpinner();
     saveUser(getUser(), (users) => {
-        console.log('Usuario Agregado ', users);
-        // users.forEach(user => addCard(user));
+        M.toast({ html: 'Usuario agregado correctamente.' }, 4000, 'blue')
         listUsers();
+        cleanForm();
     });
-    console.log('Crear Usuario');
 }
 const getUsers = (callback) => {
+    showSpinner();
     fetch(`${URL_API}/users`, {
         method: 'GET'
     })
@@ -118,31 +121,89 @@ const getUsers = (callback) => {
         .then(users => callback(users))
         .catch(e => {
             console.log('Error ', e);
+            hideSpinner();
         })
 }
+const getUserId = (id, callback) => {
+    fetch(`${URL_API}/users/${id}`)
+        .then(res => res.json())
+        .then(data => callback(data))
+        .catch(e => console.log(e))
+}
 const deleteUser = (id) => {
+    showSpinner();
     fetch(`${URL_API}/users/${id}`, {
         method: 'DELETE'
     })
-    .then(res => res.json())
-    .then(res => {
-        // alert('Registro eliminado correctamente.');
-        M.toast({html: 'Usuario eliminado correctamente.'}, 4000, 'blue')
-        listUsers();
-    })
-    .catch(e => console.log('error ',e));
+        .then(res => res.json())
+        .then(res => {
+            M.toast({ html: 'Usuario eliminado correctamente.' }, 4000, 'blue');
+            listUsers();
+        })
+        .catch(e => console.log('error ', e));
 }
+const setInputValue = (form, el, valueText) => {
+    form.querySelector(el).value = valueText;
+    form.querySelector(el).parentElement.querySelector('label').classList.add('active');
+}
+const onModalOpen = (id) => {
+    showSpinner();
+    getUserId(id, (user) => {
+        const modal = document.querySelector('#modal1');
+        const form = modal.querySelector('form');
+        setInputValue(form, '#nameModal', user.name);
+        setInputValue(form, '#lastnameModal', user.lastname);
+        setInputValue(form, '#countryModal', user.country);
+        setInputValue(form, '#mailModal', user.mail);
+        setInputValue(form, '#photoModal', user.photo);
+        setInputValue(form, '#descriptionModal', user.description);
+        hideSpinner();
+    })
+};
 const updateUser = (id) => {
-    console.log(id);
+    var elems = document.querySelector('.modal');
+    var instances = M.Modal.init(elems, {
+        dismissible: false,
+        onOpenEnd: onModalOpen(id),
+        id: id
+    });
+    instances.open();
+}
+const updateUserModal = (event) => {
+    event.preventDefault();
+    showSpinner();
+    fetch(`${URL_API}/users/${modal.M_Modal.options.id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(getUserModal())
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            modal.M_Modal.close();
+            M.toast({ html: 'Usuario editado correctamente.' }, 4000, 'blue')
+            listUsers();
+        })
+        .catch(e => console.log(e))
+}
+const closeModal = (event) => {
+    event.preventDefault();
+    modal.M_Modal.close();
 }
 const listUsers = () => {
     getUsers((users) => {
         listUsersRow.innerHTML = '';
-        users.forEach(user => addCard(user));
+        users.forEach((user, index) => {
+            addCard(user);
+            if (index == users.length - 1) {
+                hideSpinner();
+            }
+        });
     });
 }
 listUsers();
-
-btnAdd.addEventListener('click', createUser);
-
+form.addEventListener('submit', createUser);
+form.addEventListener('reset', cleanForm);
+formModal.addEventListener('submit', updateUserModal);
+formModal.addEventListener('reset', closeModal);
 
